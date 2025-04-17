f3a5286b91e4d0a872b7f5aa9d33aa82f6187af3
Klaus <113921955+rahulv13@users.noreply.github.com>
2025-04-18 00:05:41 +0530

    Update App.jsx

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./pages/Home/Home"
-import Login from "./pages/Login/Login"
-import SignUp from "./pages/SignUp/SignUp"
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 const App = () => {
+  const isLoggedIn = localStorage.getItem("token");
+
   return (
     <Router>
       <Routes>
-        <Route path="/dashboard" exact element={<Home />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/signup" exact element={<SignUp />} />
+        <Route path="/" element={
+          isLoggedIn ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+        } />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
