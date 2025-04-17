3fc80d796c5eaf0155e48588bf2631398a16c853
Klaus <113921955+rahulv13@users.noreply.github.com>
2025-04-17 23:55:01 +0530

    Update Login.jsx

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from "../../components/Navbar/Navbar"
 import { Link, useNavigate } from "react-router-dom"
-import PasswordInput from '../../components/Navbar/Input/Passwordinput'
+import PasswordInput from '../../components/Navbar/Input/PasswordInput'
 import { validateEmail } from '../../utils/helper'
 import axiosInstance from '../../utils/axiosInstance'
 
